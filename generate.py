58af45b4df6da9a20f3be548d1cf60cf82532a7a
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 21:34:23 -0600

    normal tower

--- generate.py
+++ generate.py
@@ -6,6 +6,6 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x + width, y, z + height] , size=[length, width, height])
+for i in range(10):
+    pyrosim.Send_Cube(name="Box", pos=[x, y, z+(i*height)] , size=[length, width, height])
 pyrosim.End()
