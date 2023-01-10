dd6371b2dbc4792cf7232782fc7321967522a11d
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 22:57:09 -0600

    move world link to background

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 import pyrosim.pyrosim as pyrosim
 def Create_World():
     pyrosim.Start_SDF("world.sdf")
-    pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5] , size=[1, 1, 1])  
+    pyrosim.Send_Cube(name="Box", pos=[-2, 2, 0.5] , size=[1, 1, 1])  
     pyrosim.End()
 
 def Create_Robot():
@@ -9,6 +9,6 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5] , size=[1, 1, 1])  
     pyrosim.End()
 
-    
+
 Create_World()
 Create_Robot()
