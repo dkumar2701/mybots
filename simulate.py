58ab5ef19e1ea46ee80279d43741be348a48120f
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 21:06:45 -0600

    increased steps to 100000

--- simulate.py
+++ simulate.py
@@ -6,7 +6,7 @@ p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
-for i in range(1000):
+for i in range(10000):
     p.stepSimulation()
     time.sleep(0.01)
     print(i)
