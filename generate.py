e22caf8c93fb295a40eb65f58465fdc2c65b6fe8
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 22:48:31 -0600

    one block at origin

--- generate.py
+++ generate.py
@@ -6,15 +6,9 @@ height = 1
 x = 0
 y = 0
 z = 0.5
-sumheight = 0
-for row in range(5):
-    for col in range(5):
-        length = width = height = 1
-        sumheight = 0
-        for i in range(10):
-            pyrosim.Send_Cube(name="Box", pos=[x+(row), y+(col), z+(sumheight)] , size=[length, width, height])
-            sumheight += 1 
-            length = width = height = 0.9*length
+
+pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
+
 
     
 pyrosim.End()
