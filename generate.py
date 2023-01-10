3e064a360d69fdb4a540c30b506d48f27622132b
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 21:17:49 -0600

    added x, y, z vars

--- generate.py
+++ generate.py
@@ -3,5 +3,8 @@ pyrosim.Start_SDF("box.sdf")
 length = 1
 width = 2
 height = 3
-pyrosim.Send_Cube(name="Box", pos=[0,0,0.5] , size=[length, width, height])
+x = 0
+y = 0
+z = 1.5
+pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
 pyrosim.End()
