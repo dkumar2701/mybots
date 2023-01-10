e6e24fb78215bee2a30134cbd06e7e11d8649e13
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 23:16:09 -0600

    link 4

--- generate.py
+++ generate.py
@@ -11,6 +11,10 @@ def Create_Robot():
     pyrosim.Send_Cube(name="Link1", pos=[0, 0, 0.5] , size=[1, 1, 1])
     pyrosim.Send_Joint(name = "Link1_Link2" , parent= "Link1" , child = "Link2" , type = "revolute", position = [0, 0, 1])
     pyrosim.Send_Cube(name="Link2", pos=[0, 0, 0.5] , size=[1, 1, 1])
+    pyrosim.Send_Joint(name = "Link2_Link3" , parent= "Link2" , child = "Link3" , type = "revolute", position = [0, 0.5, 0.5])
+    pyrosim.Send_Cube(name="Link3", pos=[0, 0.5, 0] , size=[1, 1, 1])
+    pyrosim.Send_Joint(name = "Link3_Link4" , parent= "Link3" , child = "Link4" , type = "revolute", position = [0, 1, 0])
+    pyrosim.Send_Cube(name="Link4", pos=[0, 0.5, 0] , size=[1, 1, 1])
     pyrosim.End()
 
 
