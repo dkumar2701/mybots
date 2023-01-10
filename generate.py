f51dae32f3c2e4efe247bee868e093821dd8b4f6
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 23:02:22 -0600

    added joint

--- generate.py
+++ generate.py
@@ -7,6 +7,8 @@ def Create_World():
 def Create_Robot():
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5] , size=[1, 1, 1])  
+    pyrosim.Send_Joint(name = "Torso_Leg" , parent= "Torso" , child = "Leg" , type = "revolute", position = [0.5, 0, 1])
+    pyrosim.Send_Cube(name="Leg", pos=[1, 0, 1.5] , size=[1, 1, 1])
     pyrosim.End()
 
 
