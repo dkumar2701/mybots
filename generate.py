202850824c2706a8e9f7e827bd91b3145dcc9fb5
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 22:53:46 -0600

    added create_robot

--- generate.py
+++ generate.py
@@ -4,4 +4,11 @@ def Create_World():
     pyrosim.Send_Cube(name="Box", pos=[0, 0, 0.5] , size=[1, 1, 1])  
     pyrosim.End()
 
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    pyrosim.Send_Cube(name="Torso", pos=[0, 0, 0.5] , size=[1, 1, 1])  
+    pyrosim.End()
+
+    
 Create_World()
+Create_Robot()
