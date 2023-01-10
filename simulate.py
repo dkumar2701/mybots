441f15a160ae77ae66e0ddfc11d16303b9d07e75
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 21:05:55 -0600

    added plane

--- simulate.py
+++ simulate.py
@@ -1,6 +1,8 @@
 import pybullet as p
+import pybullet_data
 import time
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 p.loadSDF("box.sdf")
