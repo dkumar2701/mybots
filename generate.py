2a16de3324c9bc601c6c85991bd666a4d76a4887
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-09 21:23:24 -0600

    twolinks diagonal start

--- generate.py
+++ generate.py
@@ -7,5 +7,5 @@ x = 0
 y = 0
 z = 0.5
 pyrosim.Send_Cube(name="Box", pos=[x, y, z] , size=[length, width, height])
-pyrosim.Send_Cube(name="Box2", pos=[x, y, z] , size=[length, width, height])
+pyrosim.Send_Cube(name="Box2", pos=[x + width, y, z + height] , size=[length, width, height])
 pyrosim.End()
