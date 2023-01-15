0b30d39ff76b367badb08955da727f55ab9fc92a
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 16:47:45 -0600

    Added Sensor

--- simulate.py
+++ simulate.py
@@ -8,6 +8,7 @@ p.setGravity(0,0,-9.8)
 planeId = p.loadURDF("plane.urdf")
 robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
+pyrosim.Prepare_To_Simulate(robotID)
 for i in range(10000):
     p.stepSimulation()
     backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
