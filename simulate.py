5c2b8664e48298b20235b525420e9ec67c3cec0a
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 19:34:55 -0600

    added random

--- simulate.py
+++ simulate.py
@@ -3,6 +3,7 @@ import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
 import numpy
+import random
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -10,11 +11,11 @@ planeId = p.loadURDF("plane.urdf")
 robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotID)
-backLegSensorValues = numpy.zeros(1000)
-frontLegSensorValues = numpy.zeros(1000)
+backLegSensorValues = numpy.zeros(10000)
+frontLegSensorValues = numpy.zeros(10000)
 
 
-for i in range(1000):
+for i in range(10000):
     p.stepSimulation()
     backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
@@ -22,7 +23,13 @@ for i in range(1000):
     bodyIndex = robotID,
     jointName = "Torso_BackLeg",
     controlMode = p.POSITION_CONTROL,
-    targetPosition = 0.0, 
+    targetPosition = -numpy.pi/4.0, 
+    maxForce = 500)
+    pyrosim.Set_Motor_For_Joint(
+    bodyIndex = robotID,
+    jointName = "Torso_FrontLeg",
+    controlMode = p.POSITION_CONTROL,
+    targetPosition = numpy.pi/4.0, 
     maxForce = 500)
     time.sleep(0.01)
     #print("Step Status: ", i)
