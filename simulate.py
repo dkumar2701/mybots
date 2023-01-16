66f0532a0ff32df50af6989ad38345015d3925c0
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 23:24:52 -0600

    draggable

--- simulate.py
+++ simulate.py
@@ -7,13 +7,7 @@ import constants as c
 import random
 from simulation import SIMULATION
 """
-physicsClient = p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0,0,-9.8)
 
-
-
-pyrosim.Prepare_To_Simulate(robotID)
 
 #numpy.save('data/targetAnglesBack.npy', targetAnglesBackLeg)
 #numpy.save('data/targetAnglesFront', targetAnglesFrontLeg)
