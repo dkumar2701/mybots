0c132690ab0e94f45ebce7eda79accab63cc8f8a
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 18:05:26 -0600

    Storing Sensor vals

--- simulate.py
+++ simulate.py
@@ -2,6 +2,7 @@ import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
+import numpy
 physicsClient = p.connect(p.GUI)
 p.setAdditionalSearchPath(pybullet_data.getDataPath())
 p.setGravity(0,0,-9.8)
@@ -9,10 +10,13 @@ planeId = p.loadURDF("plane.urdf")
 robotID = p.loadURDF("body.urdf")
 p.loadSDF("world.sdf")
 pyrosim.Prepare_To_Simulate(robotID)
-for i in range(10000):
+backLegSensorValues = numpy.zeros(1000)
+
+
+for i in range(1000):
     p.stepSimulation()
-    backLegTouch = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    print("BackLeg: ", backLegTouch)
+    backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
     time.sleep(0.01)
     #print("Step Status: ", i)
 p.disconnect()
+print(backLegSensorValues)
