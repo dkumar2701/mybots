6a5c9a976cfd7b9bacd74db38e228b7220ba9296
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 23:17:13 -0600

    draggable

--- simulate.py
+++ simulate.py
@@ -18,27 +18,11 @@ pyrosim.Prepare_To_Simulate(robotID)
 #numpy.save('data/targetAnglesBack.npy', targetAnglesBackLeg)
 #numpy.save('data/targetAnglesFront', targetAnglesFrontLeg)
 #exit()
-for i in range(1000):
-    p.stepSimulation()
-    c.backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
-    c.frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
-    pyrosim.Set_Motor_For_Joint(
-    bodyIndex = robotID,
-    jointName = "Torso_BackLeg",
-    controlMode = p.POSITION_CONTROL,
-    targetPosition = -c.targetAnglesBackLeg[i], 
-    maxForce = 75)
-    pyrosim.Set_Motor_For_Joint(
-    bodyIndex = robotID,
-    jointName = "Torso_FrontLeg",
-    controlMode = p.POSITION_CONTROL,
-    targetPosition = -c.targetAnglesFrontLeg[i], 
-    maxForce = 75)
-    time.sleep(1/240)
-    #print("Step Status: ", i)
+
 p.disconnect()
 print(c.backLegSensorValues)
 numpy.save('data/backLegSensorValues.npy', c.backLegSensorValues)
 numpy.save('data/frontLegSensorValues.npy', c.frontLegSensorValues)
 """
 simulation = SIMULATION()
+simulation.Run()
--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ from robot import ROBOT
 import pybullet as p
 import pyrosim.pyrosim as pyrosim
 import pybullet_data
-
+import time
 class SIMULATION:
     def __init__(self):
         self.physicsClient = p.connect(p.GUI)
@@ -12,4 +12,27 @@ class SIMULATION:
         self.world = WORLD()
         self.robot = ROBOT()
         pyrosim.Prepare_To_Simulate(self.robot.robotID)
+
+    def Run(self):
+        for i in range(1000):
+            print(i)
+            
+            p.stepSimulation()
+            #c.backLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("BackLeg")
+            #c.frontLegSensorValues[i] = pyrosim.Get_Touch_Sensor_Value_For_Link("FrontLeg")
+            #pyrosim.Set_Motor_For_Joint(
+            #bodyIndex = robotID,
+            #jointName = "Torso_BackLeg",
+            #controlMode = p.POSITION_CONTROL,
+            #targetPosition = -c.targetAnglesBackLeg[i], 
+            #maxForce = 75)
+            #pyrosim.Set_Motor_For_Joint(
+            #bodyIndex = robotID,
+            #jointName = "Torso_FrontLeg",
+            #controlMode = p.POSITION_CONTROL,
+            #targetPosition = -c.targetAnglesFrontLeg[i], 
+            #maxForce = 75)
+            time.sleep(1/240)
+            #print("Step Status: ", i)
+            
         
