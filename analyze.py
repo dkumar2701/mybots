ca4ca3a0f431cabef121f4358f39f078a02a27fe
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 18:41:19 -0600

    analyze front

--- analyze.py
+++ analyze.py
@@ -1,6 +1,8 @@
 import numpy
 import matplotlib.pyplot
 backLegSensorValues = numpy.load('data/backLegSensorValues.npy')
+frontLegSensorValues = numpy.load('data/frontLegSensorValues.npy')
 print(backLegSensorValues)
 matplotlib.pyplot.plot(backLegSensorValues)
+matplotlib.pyplot.plot(frontLegSensorValues)
 matplotlib.pyplot.show()
