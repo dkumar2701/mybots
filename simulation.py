ccb8c644bc34bcbf0fd6073b2362d6a9e2f2b0f2
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 23:06:59 -0600

    moved loads into classes

--- simulation.py
+++ simulation.py
@@ -1,14 +1,15 @@
 from world import WORLD
 from robot import ROBOT
 import pybullet as p
-import pyrosim.pyrosim
+import pyrosim.pyrosim as pyrosim
 import pybullet_data
 
 class SIMULATION:
     def __init__(self):
-        self.world = WORLD()
-        self.robot = ROBOT()
         self.physicsClient = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
-        pyrosim.Prepare_To_Simulate()
+        self.world = WORLD()
+        self.robot = ROBOT()
+        pyrosim.Prepare_To_Simulate(self.robot.robotID)
+        
