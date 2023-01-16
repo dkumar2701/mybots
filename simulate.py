af93974a9786de6954aef05d2d8d149f9a609f4e
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-16 10:59:16 -0600

    simulate.py fixed

--- simulate.py
+++ simulate.py
@@ -1,22 +1,4 @@
-import pybullet as p
-import pyrosim.pyrosim as pyrosim
-import pybullet_data
-import time
-import numpy
-import constants as c
-import random
 from simulation import SIMULATION
-"""
 
-
-#numpy.save('data/targetAnglesBack.npy', targetAnglesBackLeg)
-#numpy.save('data/targetAnglesFront', targetAnglesFrontLeg)
-#exit()
-
-p.disconnect()
-print(c.backLegSensorValues)
-numpy.save('data/backLegSensorValues.npy', c.backLegSensorValues)
-numpy.save('data/frontLegSensorValues.npy', c.frontLegSensorValues)
-"""
 simulation = SIMULATION()
 simulation.Run()
