873d11fdcc26f0d89fadd480ce6833251520e0c9
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 23:28:00 -0600

    destructor

--- robot.py
+++ robot.py
@@ -1,6 +1,7 @@
 import pybullet as p
 class ROBOT:
     def __init__(self):
+        self.robotID = p.loadURDF("body.urdf")
         self.sensors = {}
         self.motors = {}
-        self.robotID = p.loadURDF("body.urdf")
+        
