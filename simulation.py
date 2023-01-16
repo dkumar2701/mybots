bee8fa0f94407b609bf65dd01994fddce554b956
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 21:32:02 -0600

    first cut into simulation constructor

--- simulation.py
+++ simulation.py
@@ -1,7 +1,14 @@
 from world import WORLD
 from robot import ROBOT
+import pybullet as p
+import pyrosim.pyrosim
+import pybullet_data
 
 class SIMULATION:
     def __init__(self):
         self.world = WORLD()
         self.robot = ROBOT()
+        self.physicsClient = p.connect(p.GUI)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0,0,-9.8)
+        pyrosim.Prepare_To_Simulate()
