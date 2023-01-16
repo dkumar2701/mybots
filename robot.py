e741b171aefa41dd5dcbc72aeafbbe5c39fa7040
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-15 23:38:14 -0600

    print link names

--- robot.py
+++ robot.py
@@ -1,9 +1,12 @@
 import pybullet as p
+import pyrosim.pyrosim as pyrosim
 class ROBOT:
     def __init__(self):
         self.robotID = p.loadURDF("body.urdf")
-        self.sensors = {}
+        
         self.motors = {}
         
     def Prepare_To_Sense(self):
-        pass
+        self.sensors = {}
+        for linkName in pyrosim.linkNamesToIndices:
+            print(linkName)
