f428d1761d5aeb75f4168d9b901efd617aac8c7e
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-19 19:21:19 -0600

    added generate body

--- generate.py
+++ generate.py
@@ -1,10 +1,10 @@
 import pyrosim.pyrosim as pyrosim
-def Create_World():
+def Generate_Body():
+    #Generate World
     pyrosim.Start_SDF("world.sdf")
     pyrosim.Send_Cube(name="Box", pos=[-2, 2, 0.5] , size=[1, 1, 1])  
     pyrosim.End()
-
-def Create_Robot():
+    #Generate Robot
     pyrosim.Start_URDF("body.urdf")
     pyrosim.Send_Cube(name="Torso", pos=[1.5, 0, 1.5] , size=[1, 1, 1])  
     pyrosim.Send_Joint(name = "Torso_BackLeg" , parent= "Torso" , child = "BackLeg" , type = "revolute", position = [1, 0, 1])
@@ -14,6 +14,4 @@ def Create_Robot():
     
     pyrosim.End()
 
-
-Create_World()
-Create_Robot()
+Generate_Body()
