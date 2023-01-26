6725718138a82d6728a350d5aa77439264528f01
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-26 17:21:18 -0600

    rescale

--- solution.py
+++ solution.py
@@ -1,6 +1,6 @@
 import numpy
 class SOLUTION:
     def __init__(self):
-        self.weights = numpy.random.rand(3,2)
+        self.weights = (numpy.random.rand(3,2) *2) - 1
         print(self.weights)
         exit()
