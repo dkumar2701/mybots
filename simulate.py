0f30fd12e83b9dec6137c69af563bef104cf97c9
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-27 12:07:17 -0600

    added direct or gui

--- simulate.py
+++ simulate.py
@@ -1,5 +1,7 @@
 from simulation import SIMULATION
+import sys
 
-simulation = SIMULATION()
+directOrGUI = sys.argv[1]
+simulation = SIMULATION(directOrGUI)
 simulation.Run()
 simulation.Get_Fitness()
--- simulation.py
+++ simulation.py
@@ -5,8 +5,11 @@ import pyrosim.pyrosim as pyrosim
 import pybullet_data
 import time
 class SIMULATION:
-    def __init__(self):
-        self.physicsClient = p.connect(p.DIRECT)
+    def __init__(self, directOrGui):
+        if directOrGui == "DIRECT":
+            self.physicsClient = p.connect(p.DIRECT)
+        else: 
+            self.physicsClient = p.connect(p.GUI)
         p.setAdditionalSearchPath(pybullet_data.getDataPath())
         p.setGravity(0,0,-9.8)
         self.world = WORLD()
