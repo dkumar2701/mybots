e0910d17f08f68eb1e1ccd5e276213d00705fbf3
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-27 16:41:38 -0600

    remove fit after read

--- solution.py
+++ solution.py
@@ -10,17 +10,20 @@ class SOLUTION:
         self.myID = nextAvailableID
 
     def Set_ID(self, ID):
-        self.myID = ID
+        self.myID = ID      
         
-    def Evaluate(self, directOrGUI):
+    def Start_Simulation(self, directOrGUI):
         self.Create_World()
         self.Create_Body()
         self.Create_Brain()
         os.system("start /B python3 simulate.py " + directOrGUI + " " + str(self.myID) + " " )
+
+    def Wait_For_Simulation_To_End(self):
         while not os.path.exists("fitness" + str(self.myID) + ".txt"):
             time.sleep(0.01)
         f = open("fitness" + str(self.myID) + ".txt", "r")
         self.fitness = float(f.read())
+        os.system("del fitness"+ str(self.myID) + ".txt")
         print("Fitness: ", self.fitness, "\n")
         #print("Read: ", self.fitness)
 
