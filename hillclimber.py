59d9916dcfe4a8c698cbd644d323d9f9daf1a378
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-26 23:26:16 -0600

    creates CHILDS

--- hillclimber.py
+++ hillclimber.py
@@ -8,13 +8,14 @@ class HILLCLIMBER:
     
     def Evolve(self):
         self.parent.Evaluate()
-        for currentGeneration in range (c.numberOfGenerations -1):
+        for currentGeneration in range (c.numberOfGenerations):
             self.Evolve_For_One_Generation()
 
     def Evolve_For_One_Generation(self):
         self.Spawn()
         self.Mutate()
         self.child.Evaluate()
+        self.Print()
         self.Select()
             
     def Spawn(self):
@@ -23,8 +24,12 @@ class HILLCLIMBER:
     def Mutate(self):
         self.child.Mutate()
 
+    def Print(self):
+        print("Parent Fitness: ", self.parent.fitness, "  Child Fitness: ", self.child.fitness)
 
     def Select(self):
         if self.parent.fitness > self.child.fitness:
             self.parent = self.child
+        else:
+            self.parent = self.parent
     
