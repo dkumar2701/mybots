a1ff14926872337e20192af7242ee44c0d1b8344
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-27 15:02:02 -0600

    brain IDS

--- solution.py
+++ solution.py
@@ -43,7 +43,7 @@ class SOLUTION:
         pyrosim.End()
 
     def Create_Brain(self):  
-        pyrosim.Start_NeuralNetwork("brain.nndf")
+        pyrosim.Start_NeuralNetwork("brain" + str(self.myID) + ".nndf")
         pyrosim.Send_Sensor_Neuron(name = 0, linkName= "Torso")
         pyrosim.Send_Sensor_Neuron(name = 1, linkName= "BackLeg")
         pyrosim.Send_Sensor_Neuron(name = 2, linkName= "FrontLeg")
