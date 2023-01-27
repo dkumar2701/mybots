5955e2c81541c2ca7f32e465e71bdfef68c66a2d
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-27 14:30:04 -0600

    added paralllel

---
+++ parallelHillClimber.py
@@ -0,0 +1,42 @@
+from solution import SOLUTION
+import constants as c
+import copy
+
+class PARALLEL_HILL_CLIMBER:
+    def __init__(self):
+        pass
+        #self.parent = SOLUTION()
+    
+    def Evolve(self):
+        pass
+        '''
+        self.parent.Evaluate("GUI")
+        for currentGeneration in range (c.numberOfGenerations):
+            self.Evolve_For_One_Generation()
+        '''
+
+    def Evolve_For_One_Generation(self):
+        self.Spawn()
+        self.Mutate()
+        self.child.Evaluate("DIRECT")
+        self.Print()
+        self.Select()
+            
+    def Spawn(self):
+        self.child = copy.deepcopy(self.parent)
+
+    def Mutate(self):
+        self.child.Mutate()
+
+    def Print(self):
+        print("Parent Fitness: ", self.parent.fitness, "  Child Fitness: ", self.child.fitness)
+
+    def Select(self):
+        if self.parent.fitness > self.child.fitness:
+            self.parent = self.child
+        else:
+            self.parent = self.parent
+
+    def Show_Best(self):
+        #self.parent.Evaluate("GUI")
+        pass
--- search.py
+++ search.py
@@ -1,11 +1,11 @@
 import os
-from hillclimber import HILLCLIMBER
+from parallelHillClimber import PARALLEL_HILL_CLIMBER
 """
 for i in range(5):
     os.system("python3 generate.py")
     os.system("python3 simulate.py")
 """
 
-hc = HILLCLIMBER()
-hc.Evolve()
-hc.Show_Best()
+phc = PARALLEL_HILL_CLIMBER()
+phc.Evolve()
+phc.Show_Best()
