95096d1f6a11798e9c19d4048a53bf7075b8d22a
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-01-27 23:54:59 -0600

    leftlower

--- solution.py
+++ solution.py
@@ -57,9 +57,13 @@ class SOLUTION:
         pyrosim.Send_Joint(name = "FrontLeg_FrontLowerLeg", parent= "FrontLeg", child= "FrontLowerLeg", type= "revolute", position= [0, 1, 0],
             jointAxis= "1 0 0")
         pyrosim.Send_Cube(name= "FrontLowerLeg", pos= [0, 0, -0.5], size=[0.2, 0.2, 1])
-        
+
         pyrosim.Send_Joint(name="Torso_LeftLeg", parent= "Torso", child= "LeftLeg", type= "revolute", position= [-0.5, 0, 1], jointAxis= "0 1 0")
         pyrosim.Send_Cube(name= "LeftLeg", pos=[-0.5, 0, 0], size=[1, 0.2, 0.2])
+        pyrosim.Send_Joint(name = "LeftLeg_LeftLowerLeg", parent= "LeftLeg", child= "LeftLowerLeg", type= "revolute", position= [-1, 0, 0],
+            jointAxis= "0 1 0")
+        pyrosim.Send_Cube(name= "LeftLowerLeg", pos= [0, 0, -0.5], size=[0.2, 0.2, 1])
+
         pyrosim.Send_Joint(name = "Torso_RightLeg", parent= "Torso", child= "RightLeg", type= "revolute", position= [0.5, 0, 1], jointAxis= "0 1 0")
         pyrosim.Send_Cube(name= "RightLeg", pos= [0.5, 0, 0], size=[1, 0.2, 0.2])
         
