1544376eff86b26a3ef24835405e401f75392472
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-02-26 11:51:17 -0600

    works with bigger pops

--- constants.py
+++ constants.py
@@ -13,8 +13,8 @@ zdiff = 2
 preferLastChain = 3
 preferattachY = 2
 
-numberOfGenerations = 4
-populationSize = 2
+numberOfGenerations = 10
+populationSize = 10
 #numSensorNeurons = 5
 #numMotorNeurons = 11
 motorJointRange = 0.5
