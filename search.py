afcdd7aec8b7211a7ea97937eb022dd8585a00d7
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-03-10 23:48:33 -0600

    seed checker

--- search.py
+++ search.py
@@ -1,11 +1,14 @@
 import os
 from parallelHillClimber import PARALLEL_HILL_CLIMBER
+import numpy as np
 """
 for i in range(5):
     os.system("python3 generate.py")
     os.system("python3 simulate.py")
 """
-
+thisSeed = np.random.randint(0, 20)
+np.random.seed(thisSeed)
 phc = PARALLEL_HILL_CLIMBER()
 phc.Evolve()
 phc.Show_Best()
+print("The seed used was: ", thisSeed, "\n")
