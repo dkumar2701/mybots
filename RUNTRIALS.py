beb7bed3fcf0c9df54520a791f3f2b72675d48d0
dkumar2701 <divykumar2023@u.northwestern.edu>
2023-03-11 18:26:29 -0600

    code for number of trials

--- RUNTRIALS.py
+++ RUNTRIALS.py
@@ -1,4 +1,19 @@
 from multSearch import multSearch
 import constants as c
 
-print(c.numberOfGenerations)
+print("Let's Evolve some Robots!\n")
+while(True):
+    trialInput = input("Please input how many trials you would like to do: \n")
+    
+    if trialInput == "":
+        print("You need to give me an integer")
+    else:
+        try: 
+            val = int(trialInput)
+        except ValueError:
+            print("You need to give me an integer")
+        else: 
+            break
+
+searchNum = int(trialInput)
+
